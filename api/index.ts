8b5fd82939b5eb98a02bca58d6d3e1f4b2ffc9a2
Coinacci <yysoncul@gmail.com>
2026-03-06 13:21:32 +0300

    Fix Vercel serverless entry point

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,5 @@
-// Vercel serverless function entry point
 import { createApp } from '../server/createApp';
-import { registerRoutes } from '../server/routes';
 
 const app = createApp();
-await registerRoutes(app);
 
 export default app;
