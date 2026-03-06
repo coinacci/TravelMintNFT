0ab504ed3390942a951b0df742be613fb48fb61b
Coinacci <yysoncul@gmail.com>
2026-03-06 18:46:37 +0300

    Simplify api entry point

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,10 @@
-import { createApp } from '../server/createApp';
+import express from "express";
+import { registerRoutes } from '../server/routes';
 
-const app = createApp();
+const app = express();
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
+
+registerRoutes(app);
 
 export default app;
